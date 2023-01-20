20878a63440988f7fb31a66aab71221d2b9390f7
Benjamin B <dev@benjamin-b.fr>
2023-01-20 16:36:28 +0100

    Write in csv file

--- main.py
+++ main.py
@@ -1,56 +1,88 @@
+from collections import UserDict
 import csv
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://books.toscrape.com/catalogue/the-project_856/index.html'
+url = "https://books.toscrape.com/catalogue/the-project_856/index.html"
 
-class Produit():   
+
+class Produit(UserDict):
     def __init__(self, page_url, soup):
+        self.data = {}
+        
         self.page_url = page_url
-        self.title = ''
-        self.universal_product_code = ''
+        self.title = ""
+        self.universal_product_code = ""
         self.price_excluding_tax = 0
         self.price_including_tax = 0
         self.number_available = 0
-        self.product_description = ''
-        self.category = ''
+        self.product_description = ""
+        self.category = ""
 
         self.review_rating = 0
-        self.image_url = ''
+        self.image_url = ""
 
         body = soup.html.body
 
-        self.title = body.find('h1')
-        self.product_description = body.find(id='product_description').next_sibling.next_sibling.text
+        self.title = body.find("h1").text
+        self.data["title"] = body.find("h1").text
 
-        breadcrumb = body.find('ul','breadcrumb')
-        self.category = breadcrumb.find_all('li')[-2].text
+        titre_description = body.find(id="product_description")
+        self.product_description = titre_description.next_sibling.next_sibling.text
 
-        star_rating = body.find('p','star-rating')['class'][-1]
-        trad_numbers = {'One':1, 'Two':2, 'Three':3, 'Four':4, 'Five':5}
+        breadcrumb = body.find("ul", "breadcrumb")
+        self.category = breadcrumb.find_all("li")[-2].text
+
+        star_rating = body.find("p", "star-rating")["class"][-1]
+        trad_numbers = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
         self.review_rating = trad_numbers[star_rating]
 
-        self.image_url = body.find(id='product_gallery').find('img')['src']
+        self.image_url = body.find(id="product_gallery").find("img")["src"]
 
-        table_details = body.find('table','table-striped')
-        correspondance_table = {'UPC': 'universal_product_code',
-            'Price (excl. tax)': 'price_excluding_tax',
-            'Price (incl. tax)': 'price_including_tax',
-            'Availability': 'number_available',
+        table_details = body.find("table", "table-striped")
+        correspondance_table = {
+            "UPC": "universal_product_code",
+            "Price (excl. tax)": "price_excluding_tax",
+            "Price (incl. tax)": "price_including_tax",
+            "Availability": "number_available",
         }
-        for ligne in table_details.find_all('tr') :
+        for ligne in table_details.find_all("tr"):
             cle = ligne.th.text
             valeur = ligne.td.text
 
             if cle in correspondance_table:
-                setattr(self,correspondance_table[cle],valeur)
-        
+                setattr(self, correspondance_table[cle], valeur)
+
+
 def main():
+    chemin_sortie = "Data/data.csv"
+    headers = [
+        "product_page_url",
+        "universal_ product_code",
+        "title",
+        "price_including_tax",
+        "price_excluding_tax",
+        "number_available",
+        "product_description",
+        "category",
+        "review_rating",
+        "image_url",
+    ]
+
     reponse = requests.get(url)
     if reponse.ok:
         soup = BeautifulSoup(reponse.text, features="html.parser")
         produit = Produit(url, soup)
-        #print(produit.image_url)
+        print(produit["title"])
 
+        with open(chemin_sortie, "w", encoding="utf-8", newline="") as output_csv:
+            writer = csv.DictWriter(
+                output_csv,
+                fieldnames=headers,
+                restval="Not Found",
+                extrasaction="ignore",
+            )
+            writer.writeheader()
+            writer.writerow(produit.data)
 
 main()
