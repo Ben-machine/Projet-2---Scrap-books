c3773cf88498ed216e84a4544f9d3e186e09e4b6
Benjamin B <dev@benjamin-b.fr>
2023-01-21 12:11:31 +0100

    Internalisation construct Produit

--- main.py
+++ main.py
@@ -5,21 +5,42 @@ from bs4 import BeautifulSoup
 
 url = "https://books.toscrape.com/catalogue/the-project_856/index.html"
 
-
 class Produit(UserDict):
-    def __init__(self, page_url, soup):
-        self.soup = soup
+    headers = [
+            "product_page_url",
+            "universal_product_code",
+            "title",
+            "price_including_tax",
+            "price_excluding_tax",
+            "number_available",
+            "product_description",
+            "category",
+            "review_rating",
+            "image_url",
+        ]
+    
+    def __init__(self, page_url, header_extend = None ):
         self.data = {'product_page_url': page_url}
 
-        self.data["title"] = self.scrap_title()
-        self.data["product_description"] = self.scrap_product_description()
-        self.data["category"] = self.scrap_category()
-        self.data["review_rating"] = self.scrap_review_rating()
-        self.data["image_url"] = self.scrap_image_url()
-        self.data["universal_product_code"] = self.scrap_universal_product_code()
-        self.data["price_excluding_tax"] = self.scrap_price_excluding_tax()
-        self.data["price_including_tax"] = self.scrap_price_including_tax()
-        self.data["number_available"] = self.scrap_number_available()
+        if header_extend is not None:
+            self.headers.extend(header_extend)
+
+        reponse = requests.get(url)
+        if reponse.ok:
+            self.soup = BeautifulSoup(reponse.text, features="html.parser")
+
+            self.data["title"] = self.scrap_title()
+            self.data["product_description"] = self.scrap_product_description()
+            self.data["category"] = self.scrap_category()
+            self.data["review_rating"] = self.scrap_review_rating()
+            self.data["image_url"] = self.scrap_image_url()
+            self.data["universal_product_code"] = self.scrap_universal_product_code()
+            self.data["price_excluding_tax"] = self.scrap_price_excluding_tax()
+            self.data["price_including_tax"] = self.scrap_price_including_tax()
+            self.data["number_available"] = self.scrap_number_available()
+        else:
+            print('Erreur de requete')
+            #Statut d'erreur ?
 
     def scrap_title(self):
         return self.soup.html.body.find("h1").text.strip()
@@ -59,32 +80,17 @@ class Produit(UserDict):
 
 def main():
     chemin_sortie = "Data/data.csv"
-    headers = [
-        "product_page_url",
-        "universal_product_code",
-        "title",
-        "price_including_tax",
-        "price_excluding_tax",
-        "number_available",
-        "product_description",
-        "category",
-        "review_rating",
-        "image_url",
-    ]
+    
+    produit = Produit(url)
 
-    reponse = requests.get(url)
-    if reponse.ok:
-        soup = BeautifulSoup(reponse.text, features="html.parser")
-        produit = Produit(url, soup)
-
-        with open(chemin_sortie, "w", encoding="utf-8", newline="") as output_csv:
-            writer = csv.DictWriter(
-                output_csv,
-                fieldnames=headers,
-                restval="Not Found",
-                extrasaction="ignore",
-            )
-            writer.writeheader()
-            writer.writerow(produit.data)
+    with open(chemin_sortie, "w", encoding="utf-8", newline="") as output_csv:
+        writer = csv.DictWriter(
+            output_csv,
+            fieldnames=Produit.headers,
+            restval="Not Found",
+            extrasaction="ignore",
+        )
+        writer.writeheader()
+        writer.writerow(produit.data)
 
 main()
