ac3e154900db538729a5ee1c4ffb2b6ec2a50a48
Benjamin B <dev@benjamin-b.fr>
2023-01-30 18:22:39 +0100

    Add doc to utils.py

--- utils.py
+++ utils.py
@@ -4,17 +4,20 @@ import config
 from functools import wraps
 
 def get_data_folder():
+    """Retourne le chemin vers le sous-dossier horodaté où stocker les données 
+        après l'avoir créé."""
     time_code = datetime.now().strftime("%y%m%d%H%M%S")
     dir_path = f"{config.DIR_DATA}/{time_code}"
     os.makedirs(dir_path)
     return dir_path
 
 def time_exec(func):
+    """Decorateur affichant le temps d'execution du script."""
     @wraps(func)
     def time_exec_wrapper(*args, **kwargs):
         debut = datetime.now()
         result = func(*args, **kwargs)
         fin = datetime.now()
-        print(f"Données extraites en {fin - debut}")
+        print(f"Execution en {fin - debut}")
         return result     
     return time_exec_wrapper
